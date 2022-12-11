da0f4f1e14cd7379425357c346587cc7110820b8
Deon Liang <deonyuhaoliang@gmail.com>
2022-12-11 13:34:11 -0800

    init commit portfolio migration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,56 +1,12 @@
 /**
  * Configure your Gatsby site with this file.
  *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
  */
-require('dotenv').config({
-  path: '.env'
-})
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
 module.exports = {
-  siteMetadata: {
-    title: "Deon's Portfolio",
-    author: 'Deon Liang',
-    siteUrl: 'https://deonliang.com'
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-
-    },
-    'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'src',
-        path: `${__dirname}/src/`
-      }
-    },
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {},
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 750,
-              linkImagesToOriginal: false
-            }
-          }
-
-        ]
-      }
-
-    }
-  ],
+  plugins: [],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,5 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../components/Layout'
-import Head from '../components/Head'
-import indexStyles from './index.module.scss'
-const IndexPage = () => {
-  return (
-    <Layout>
-      <Head title="Home" url="https://deonliang.com" />
-      <h1>Hello, my name is Deon</h1>
-      <h2>I am a software developer currently based in Canada.</h2>
-      <h3>I am currently looking for full time opportunities.</h3>
-      <h3>You can find out more about me <Link className={indexStyles.homeLink}to="/about">here</Link></h3>
-      <h3>You can see what kind of projects I have done <Link className={indexStyles.homeLink} to="/projects">here</Link></h3>
-    </Layout >
-  )
+import * as React from "react"
 
+export default function Home() {
+  return <div>Hello world!</div>
 }
-
-export default IndexPage
